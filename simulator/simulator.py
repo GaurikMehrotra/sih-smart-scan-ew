a9567f993a7ebfe8bc1115d7b2ed8d2c8610150a
GAURIKMEHROTRA <gaurikmehrotra245@gmail.com>
2026-09-02 18:48:20 +0000

    Added periodic and burst emitter simulation

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -1,11 +1,29 @@
 import pandas as pd
-import random
 
 data = []
 
 for time_step in range(1, 101):
+
     for band in range(1, 11):
-        active = random.randint(0, 1)
+
+        active = 0
+
+        # Band 1 -> Periodic Emitter
+        if band == 1:
+            if time_step % 5 == 0:
+                active = 1
+
+        # Band 2 -> Burst Emitter
+        elif band == 2:
+            if 20 <= time_step <= 30:
+                active = 1
+            elif 60 <= time_step <= 70:
+                active = 1
+
+        # Band 3 -> Frequency Hopper
+        elif band == 3:
+            if time_step % 4 == 0:
+                active = 1
 
         data.append({
             "time": time_step,
@@ -17,5 +35,5 @@ df = pd.DataFrame(data)
 
 df.to_csv("simulator/dataset.csv", index=False)
 
-print("Dataset created successfully!")
+print("Dataset generated successfully!")
 print(df.head())
