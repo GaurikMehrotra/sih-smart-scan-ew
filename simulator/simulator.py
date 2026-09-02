5ab2b022075fdb6c74b62eb14c37fa3874a75383
GAURIKMEHROTRA <gaurikmehrotra245@gmail.com>
2026-09-02 01:20:28 +0000

    Created basic RF simulator

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -1,1 +1,21 @@
+import pandas as pd
+import random
 
+data = []
+
+for time_step in range(1, 101):
+    for band in range(1, 11):
+        active = random.randint(0, 1)
+
+        data.append({
+            "time": time_step,
+            "band": band,
+            "active": active
+        })
+
+df = pd.DataFrame(data)
+
+df.to_csv("simulator/dataset.csv", index=False)
+
+print("Dataset created successfully!")
+print(df.head())
